624cd34df7f7ec625de05ce59d7daf3d1325c47a
codingblazer <sasachin04@gmail.com>
2018-03-15 14:25:16 +0530

    Adds import exports using webpack

---
+++ src/person.js
@@ -0,0 +1,6 @@
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 21;
+const isSeniorCitizen = (age) => age >= 65;
+
+export { isAdult, canDrink, isSeniorCitizen as default };
+//here isSeniorCitizen is default export 
