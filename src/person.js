243e2b655a393618a12133b891a4effa602beb4a
codingblazer <sasachin04@gmail.com>
2018-03-15 17:01:41 +0530

    Deleted some play files and moved main app.js code

--- src/person.js
+++
@@ -1,6 +0,0 @@
-const isAdult = (age) => age >= 18;
-const canDrink = (age) => age >= 21;
-const isSeniorCitizen = (age) => age >= 65;
-
-export { isAdult, canDrink, isSeniorCitizen as default };
-//here isSeniorCitizen is default export 
